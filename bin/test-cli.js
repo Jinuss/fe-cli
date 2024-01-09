6a5baef4db5777faaea7dc9677c17dea5c0c138b
sunqi <sunqi@leador.com.cn>
2024-01-09 17:52:12 +0800

    update

--- bin/test-cli.js
+++ bin/test-cli.js
@@ -1,12 +1,14 @@
 #!/ usr/bin/env node
-console.log("hello world")
+console.log("hello Leador")
 
 const program = require('commander')
 
 const chalk = require('chalk')
 
 program
+    .version('1.0.0')
     .command('create <app-name>')
+    .alias("init")
     .description((chalk.cyan('create a new project')))
     .action((name, options) => {
         console.log("project name:", name)
